2d4e08d9a5f6317f751b2dd686a9a9fc4025bb9a
zamgi <denisov72@mail.ru>
2017-01-20 13:56:46 +0300

    Add files via upload

---
+++ --clear-after-build--.cmd
@@ -0,0 +1,5 @@
+del "*.suo" /Q 2> nul
+del "MsSqlServerDatabaseTablesGraph.WebApp\*.pdb" /Q 2> nul
+rd "MsSqlServerDatabaseTablesGraph.WebApp\obj" /S/Q 2> nul
+rd "TestApp\bin" /S/Q 2> nul
+rd "TestApp\obj" /S/Q 2> nul
