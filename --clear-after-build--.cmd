8db08f87b8b1996b3feed4b9fd27c5c30ec25e5d
zamgi <denisov72@mail.ru>
2017-01-20 14:22:20 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,5 +1,6 @@
 del "*.suo" /Q 2> nul
-del "MsSqlServerDatabaseTablesGraph.WebApp\*.pdb" /Q
+del "MsSqlServerDatabaseTablesGraph.WebApp\bin\*.pdb" /Q
+del "MsSqlServerDatabaseTablesGraph.WebApp\*.csproj.user" /Q
 rd "MsSqlServerDatabaseTablesGraph.WebApp\obj" /S/Q
 rd "TestApp\bin" /S/Q
 rd "TestApp\obj" /S/Q
