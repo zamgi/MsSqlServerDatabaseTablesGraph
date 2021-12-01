aab9afd5a087da8c6f009858f77efeb4914e3886
zamgi <denisov72@mail.ru>
2021-12-01 20:44:57 +0300

    Delete --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++
@@ -1,6 +0,0 @@
-rd ".vs" /S/Q
-del "*.csproj.user" /S/Q/F/A
-del "MsSqlServerDatabaseTablesGraph.WebApp\bin\*.pdb" /Q
-rd "MsSqlServerDatabaseTablesGraph.WebApp\obj" /S/Q
-rd "TestApp\bin" /S/Q
-rd "TestApp\obj" /S/Q
